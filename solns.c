75ec5a11c2d1a12158809caa11e4569b314c6a09
K-Amarnath <85100911+K-Amarnath@users.noreply.github.com>
2021-06-19 08:31:17 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,26 @@
-/* Enter your solutions in this file */
 #include <stdio.h>
+#include <assert.h>
+
+
+float average(int [], int);
+
+int main(void) 
+{
+    int x[] = {9,5,6,10,2,-3,4};
+    assert ((average(x, 7) - 4.7142) < 0.001);
+
+    int y[] = {5};
+    assert (average(y, 1) == 5.0);
+    int c = sizeof(x)/sizeof(x[0]);
+    printf("Average: passed\n");
+}
+
+float average(int x[], int c)
+{
+    int k = 0;
+    for (int i = 0; i < c; i++)
+    {
+        k += x[i];
+    }
+    return k/c;
+}
